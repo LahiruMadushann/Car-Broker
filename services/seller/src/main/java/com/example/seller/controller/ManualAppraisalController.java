69c294cca298e09a1af3446c85e45fa0606348b9
LahiruMadushann <lahirumadushandl@gmail.com>
2025-04-19 11:01:03 +0530

    implement gateway service

--- services/seller/src/main/java/com/example/seller/controller/ManualAppraisalController.java
+++ services/seller/src/main/java/com/example/seller/controller/ManualAppraisalController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/appraisal/manual")
+@RequestMapping("/api/v1/seller/appraisal/manual")
 @RequiredArgsConstructor
 public class ManualAppraisalController {
     private final ManualAppraisalService manualAppraisalService;
